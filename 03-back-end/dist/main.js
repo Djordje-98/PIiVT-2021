1fa543e9643c5514c1131906ebc6fa89b3a02d11
Djordje Grkovic <grkdjordje@gmail.com>
2021-05-23 16:43:17 +0200

    create mechanism to get a single category(mock)

--- 03-back-end/dist/main.js
+++ 03-back-end/dist/main.js
@@ -1,3 +1,25 @@
-let temp1 = "Djordje";
-console.log(temp1);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const cors = require("cors");
+const dev_1 = require("./config/dev");
+const service_1 = require("./components/category/service");
+const controller_1 = require("./components/category/controller");
+const application = express();
+application.use(cors());
+application.use(express.json());
+application.use(dev_1.default.server.static.route, express.static(dev_1.default.server.static.path, {
+    index: dev_1.default.server.static.index,
+    cacheControl: dev_1.default.server.static.cacheControl,
+    maxAge: dev_1.default.server.static.maxAge,
+    etag: dev_1.default.server.static.etag,
+    dotfiles: dev_1.default.server.static.dotfiles,
+}));
+const categoryService = new service_1.default();
+const categoryController = new controller_1.default(categoryService);
+application.get("/category", categoryController.getAll.bind(categoryController));
+application.use((req, res) => {
+    res.sendStatus(404);
+});
+application.listen(dev_1.default.server.port);
 //# sourceMappingURL=main.js.map
